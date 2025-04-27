f1706f83336eb000c4e45a7cc80b72c1b90f8aae
Marko Vujanic <crashxx@gmail.com>
2025-04-27 17:31:17 +0200

    add fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../style/main.css';
 </script>
 
-<a class="visually-hidden button" href="#main-content">Skip to content</a>
+<a class="visually-hidden button font-base" href="#main-content">Skip to content</a>
 <main class="mt-m-l" tabindex="-1" id="main-content">
 	<slot />
 </main>
